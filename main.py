446789960bcfbaed12198b594ad59ddd00e46ec2
Kimi <zheweiwan@gmail.com>
2022-05-17 22:29:03 -0700

    Added 2 new things

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import json
 from termcolor import cprint
 
 from manage import Manage
-from user import login, register
+from user import login, register, change_acc_details
 
 
 def clear():
@@ -14,6 +14,7 @@ def clear():
 # Trying to fix the json file if there aren't any shopping cart users
 # For some reason, it fixes itself AFTER a restart on the script
 # I can't fix this in any way because Python is just like this so ye :p
+
 with open('shopping_cart.json', 'r') as shopping_cart:
     shopping_cart = json.load(shopping_cart)
     if not shopping_cart:
@@ -26,6 +27,169 @@ with open('shopping_cart.json', 'r') as shopping_cart:
                 for user in users["Shoppers"].keys():
                     stuff[user] = {}
             json.dump(stuff, f, indent=4)
+
+
+def admin_panel(name, password):
+    manage = Manage(name, admin=True)
+    while True:
+        choice = input("Press Enter anytime to exit\n"
+                       "1. Search for item\n"
+                       "2. Manage Items\n"
+                       "3. Manage Account\n"
+                       "4. Switch to Shopping mode\n"
+                       "Enter the number: ")
+        if choice:
+
+            if choice == "1":
+                while True:
+                    item = input("Enter prefix/item name to search (be slightly specific): ")
+                    if item:
+                        managed = manage.search_item(item)
+                        if managed:
+                            for i in range(len(managed)):
+                                print(f"{i + 1}. {managed[i][0]}: ${managed[i][1]}")
+                        else:
+                            cprint("No related results!", color='yellow')
+                    else:
+                        break
+
+            elif choice == "2":
+                while True:
+                    choice = input("1. Add item\n2. Delete item\nChoice: ")
+                    if not choice:
+                        break
+                    if choice == "1" or choice == "2":
+                        manage.manage_item(choice)
+                        clear()
+                    else:
+                        cprint("Not an option!", color='yellow')
+                        time.sleep(2)
+                    time.sleep(2)
+                    clear()
+
+            elif choice == "3":
+                print(f"Username: {name}\nPassword: {password}")
+                new_username = input("New Username:")
+                new_password = input("New Password: ")
+                if new_username or new_password:
+                    change_acc_details(name, new_username, new_password)
+                    cprint("Success!", color="green")
+                    time.sleep(2)
+                    clear()
+
+            elif choice == "4":
+                return "switch_mode"
+
+            else:
+                cprint("That's not an option.", color='red')
+        else:
+            return
+
+
+def shopper_panel(name, password):
+    manage = Manage(name, admin=False)
+    while True:
+        choice = input("1. Add to Shopping Cart\n"
+                       "2. Remove Item from Shopping Cart\n"
+                       "3. View Your Shopping Cart\n"
+                       "4. Search for item\n"
+                       "5. Manage Account\n"
+                       "6. Switch to Admin mode (admin account required)\n"
+                       "Enter the number: ")
+
+        if choice:
+
+            if choice == "1":
+                while True:
+                    item = input("Enter Item name: ")
+                    if item:
+                        managed = manage.add_shopping_cart(item)
+                        if managed:
+                            print(f"{item.title()} successfully added to shopping cart!")
+                        else:
+                            print("There's no such item (very sensitive system)!")
+                        time.sleep(2)
+                        clear()
+                        break
+
+                    else:
+                        break
+
+            elif choice == "2":
+                while True:
+                    item = input("Enter item name to remove: ")
+                    item = item.title()
+                    if item:
+                        managed = manage.delete_shopping_cart(item)
+                        if managed is True:
+                            print(f"{item} was successfully removed to your shopping cart!")
+                            time.sleep(2)
+                            clear()
+                            break
+                        elif managed is False:
+                            print("There's no such item (very sensitive system)!")
+                            time.sleep(2)
+                            clear()
+                        elif managed is None:
+                            print("You don't have that item in your shopping cart!")
+                            time.sleep(2)
+                            clear()
+
+                    else:
+                        clear()
+                        break
+
+            elif choice == "3":
+                managed = manage.view_shopping_cart()
+                if managed:
+                    clear()
+                    print("Your Shopping Cart:")
+                    for i in managed.keys():
+                        print(f"{i}: ${managed[i][1]}")
+                    input("Press Enter to Continue...")
+                    clear()
+
+                else:
+                    cprint("You don't have anything in your shopping cart!", color='yellow')
+                    time.sleep(2)
+
+            elif choice == "4":
+                while True:
+                    item = input("Enter prefix/item name to search (be slightly specific): ")
+                    if item:
+                        managed = manage.search_item(item)
+                        if managed:
+                            for i in range(len(managed)):
+                                cprint(f"{i + 1}. {managed[i][0]}: ${managed[i][1]}", color='green')
+                        else:
+                            cprint("No related results!", color='yellow')
+                    else:
+                        break
+
+            elif choice == "5":
+                print(f"Username: {name}\nPassword: {password}")
+                new_username = input("New Username:")
+                new_password = input("New Password: ")
+                if new_username or new_password:
+                    change_acc_details(name, new_username, new_password)
+                    cprint("Success!", color="green")
+                    time.sleep(2)
+                    clear()
+
+            elif choice == "6":
+                with open("users.json", 'r') as user_file:
+                    users_ = json.load(user_file)
+                    if name in users_["Managers"].keys():
+                        return "switch_mode"
+                    else:
+                        cprint("You don't have permission!", color="red")
+                        input("Press Enter to continue...")
+
+            else:
+                cprint("That's not an option!", color='red')
+        else:
+            return
+        clear()
 
 
 def main():
@@ -50,7 +214,6 @@ def main():
                     if check == "Admin" or check == "Shopper":
                         if check == "Admin":
                             admin = True
-                        name = username
                         print("Login Success!")
                         time.sleep(2)
                         flag = True
@@ -72,7 +235,6 @@ def main():
                         break
                     check = register(username, password)
                     if check:
-                        name = username
                         cprint("Register Success!", color='green')
                         flag = True
                         time.sleep(2)
@@ -92,190 +254,22 @@ def main():
         if flag is True:
             break
 
+    switch = False
     while True:
-        # TODO: ADMIN -> SHOPPER MODE WHEN (AND VICE VERSA)???
-        if admin:  # If the user is an admin
-            manage = Manage(name, admin=True)
-            while True:
-                choice = input("Press Enter anytime to exit\n"
-                               "1. Add to Shopping Cart\n"
-                               "2. Remove from Shopping Cart\n"
-                               "3. View Your Shopping Cart\n"
-                               "4. Search for item\n"
-                               "5. Manage Items\n"
-                               "Enter the number: ")
-                if choice:
-                    if choice == "1":
-                        while True:
-                            item = input("Enter item name to add: ")
-                            item = item.title()
-                            if item:
-                                managed = manage.add_shopping_cart(item)
-                                if managed:
-                                    cprint(f"{item.title()} successfully added to shopping cart!", color='green')
-                                elif managed is None:
-                                    cprint("There's no such item (very sensitive system)!", color='red')
-                                elif managed is False:
-                                    print("You already have this item in your shopping cart!")
-                                time.sleep(2)
-                                clear()
-                                break
-                            else:
-                                clear()
-                                break
+        if admin and switch is False:  # If the user is an admin
+            admin = admin_panel(username, password)
+            if admin == "switch_mode":
+                switch = True
+                print("Switching...")
+                time.sleep(1)
+                clear()
 
-                    elif choice == "2":
-                        while True:
-                            item = input("Enter item name to remove: ")
-                            item = item.title()
-                            if item:
-                                managed = manage.delete_shopping_cart(item)
-                                if managed is True:
-                                    print(f"{item} was successfully removed to your shopping cart!")
-                                    time.sleep(2)
-                                    clear()
-                                    break
-                                elif managed is False:
-                                    print("There's no such item (very sensitive system)!")
-                                    time.sleep(2)
-                                    clear()
-                                elif managed is None:
-                                    print("You don't have that item in your shopping cart!")
-                                    time.sleep(2)
-                                    clear()
-
-                            else:
-                                clear()
-                                break
-
-                    elif choice == "3":
-                        managed = manage.view_shopping_cart()
-                        if managed:
-                            clear()
-                            print("Your Shopping Cart:")
-                            for i in managed.keys():
-                                print(f"{i}: ${managed[i][1]}")
-                            input("Press Enter to Continue...")
-                            clear()
-
-                        else:
-                            cprint("You don't have anything in your shopping cart!", color='yellow')
-                            time.sleep(2)
-
-                    elif choice == "4":
-                        while True:
-                            item = input("Enter prefix/item name to search (be slightly specific): ")
-                            if item:
-                                managed = manage.search_item(item)
-                                if managed:
-                                    for i in range(len(managed)):
-                                        print(f"{i+1}. {managed[i][0]}: ${managed[i][1]}")
-                                else:
-                                    cprint("No related results!", color='yellow')
-                            else:
-                                break
-
-                    elif choice == "5":
-                        while True:
-                            choice = input("1. Add item\n2. Delete item\nChoice: ")
-                            if not choice:
-                                break
-                            if choice == "1" or choice == "2":
-                                manage.manage_item(choice)
-                                clear()
-                            else:
-                                cprint("Not an option!", color='yellow')
-                                time.sleep(2)
-                            time.sleep(2)
-                            clear()
-
-                    else:
-                        cprint("That's not an option.", color='red')
-                else:
-                    return
-
-        else:  # If the user is a normal user
-            manage = Manage(name, admin=False)
-            while True:
-                choice = input("1. Add to Shopping Cart\n"
-                               "2. Remove Item from Shopping Cart\n"
-                               "3. View Your Shopping Cart\n"
-                               "4. Search for item\n"
-                               "Enter the number: ")
-
-                if choice:
-
-                    if choice == "1":
-                        while True:
-                            item = input("Enter Item name: ")
-                            if item:
-                                managed = manage.add_shopping_cart(item)
-                                if managed:
-                                    print(f"{item.title()} successfully added to shopping cart!")
-                                else:
-                                    print("There's no such item (very sensitive system)!")
-                                time.sleep(2)
-                                clear()
-                                break
-
-                            else:
-                                break
-
-                    elif choice == "2":
-                        while True:
-                            item = input("Enter item name to remove: ")
-                            item = item.title()
-                            if item:
-                                managed = manage.delete_shopping_cart(item)
-                                if managed is True:
-                                    print(f"{item} was successfully removed to your shopping cart!")
-                                    time.sleep(2)
-                                    clear()
-                                    break
-                                elif managed is False:
-                                    print("There's no such item (very sensitive system)!")
-                                    time.sleep(2)
-                                    clear()
-                                elif managed is None:
-                                    print("You don't have that item in your shopping cart!")
-                                    time.sleep(2)
-                                    clear()
-
-                            else:
-                                clear()
-                                break
-
-                    elif choice == "3":
-                        managed = manage.view_shopping_cart()
-                        if managed:
-                            clear()
-                            print("Your Shopping Cart:")
-                            for i in managed.keys():
-                                print(f"{i}: ${managed[i][1]}")
-                            input("Press Enter to Continue...")
-                            clear()
-
-                        else:
-                            cprint("You don't have anything in your shopping cart!", color='yellow')
-                            time.sleep(2)
-
-                    elif choice == "4":
-                        while True:
-                            item = input("Enter prefix/item name to search (be slightly specific): ")
-                            if item:
-                                managed = manage.search_item(item)
-                                if managed:
-                                    for i in range(len(managed)):
-                                        cprint(f"{i+1}. {managed[i][0]}: ${managed[i][1]}", color='green')
-                                else:
-                                    cprint("No related results!", color='yellow')
-                            else:
-                                break
-
-                    else:
-                        cprint("That's not an option!", color='red')
-                else:
-                    return
+        elif switch is True:  # If the user is a normal user
+            shopper = shopper_panel(username, password)
+            if shopper == "switch_mode" and switch is True:
+                switch = False
+                print("Switching...")
+                time.sleep(1)
                 clear()
 
 
--- user.py
+++ user.py
@@ -28,3 +28,38 @@ def register(username, password):
             with open("shopping_cart.json", 'w') as sc_2:
                 json.dump(sc, sc_2, indent=4)
         return True
+
+
+def change_acc_details(original_username, new_username, password):
+    with open("users.json", 'r') as f:
+        users = json.load(f)
+        manager = False
+        if original_username in users["Managers"].keys():
+            manager = True
+            user_info = users["Managers"][original_username]
+        elif original_username in users["Shoppers"].keys():
+            user_info = users["Shoppers"][original_username]
+
+        with open("users.json", 'w') as write_users:
+            if new_username:
+                if manager:
+                    users["Managers"].pop(original_username, None)
+                    if password:
+                        users["Managers"][new_username] = [password]
+                    else:
+                        users["Managers"][new_username] = user_info
+
+                else:
+                    users["Shoppers"].pop(original_username, None)
+                    if password:
+                        users["Shoppers"][new_username] = [password]
+                    else:
+                        users["Shoppers"][new_username] = user_info
+
+            elif password and not new_username:
+                if manager:
+                    users["Managers"][original_username] = [password]
+
+                else:
+                    users["Shoppers"][original_username] = [password]
+            json.dump(users, write_users, indent=4)
